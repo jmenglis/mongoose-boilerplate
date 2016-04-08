7fce2169e93a18d0ac352f0c2930e0a13825f486
Josh English <josh@joshenglish.com>
2016-04-08 09:22:29 -0500

    Sending Boilerplate up

--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,48 @@ var express = require('express');
 var controller = express.Router();
 
 // grab my model
-//var Model = require('../models/MyModel');
+var Taco = require('../models/Taco');
 
 /* GET home page. */
 controller.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Taco.find(function(err, tacos) {
+    if (err) return next(err);
+    res.json(tacos)
+  });
 })
+.post('/', function(req, res, next) {
+  // req.body gives us page data. req.params gives us url
+  Taco.create(req.body, function(err, taco) {
+    if (err) return next(err);
+    res.json(taco);
+  });
+})
+.get('/:id', function(req, res, next) {
+  Taco.findById(req.params.id, function(err, taco) {
+    if (err) return next(err);
+    res.json(taco);
+  });
+})
+// modern browswers accept patch the future
+.patch('/:id', function(req, res, next) {
+  Taco.findByIdAndUpdate(req.params.id, req.body, function(err, taco) {
+    if (err) return next(err);
+    res.json(taco);
+  });
+})
+.put('/:id', function(req, res, next) {
+  Taco.findByIdAndUpdate(req.params.id, req.body,  function(err, taco) {
+    if (err) return next(err);
+    res.json(taco);
+  });
+})
+.delete('/:id', function(req, res, next) {
+  Taco.findByIdAndRemove(req.params.id, function(err, taco) {
+    if (err) return next(err);
+    res.json(taco);
+  });
+});
+
 // .get('/api', function(req, res, next) {
 //   // get ALL of the types from my TYPES collection
 //   // .find has an error and maybe types that we want.
--- models/Taco.js
+++ models/Taco.js
@@ -5,4 +5,11 @@ var mongoose = require('mongoose'); // our handy dandy ORM
 // using a Schema - this is similar declaring
 // table columns etc using Migration
 
+var tacoSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  eatin: Boolean
+});
+
 // we export a Mongoose Model with the Schema
+module.exports = mongoose.model("Taco", tacoSchema);
